9bb989008417f6a05deb7ad73e093d6f681187d6
“Yantong <yfeng33@cs.washington.edu>
2024-02-09 16:52:45 -0800

    P2 Checkpoint 2 Test AVLTree

--- src/main/java/datastructures/dictionaries/AVLTree.java
+++ src/main/java/datastructures/dictionaries/AVLTree.java
@@ -27,5 +27,134 @@ import cse332.datastructures.trees.BinarySearchTree;
  */
 
 public class AVLTree<K extends Comparable<? super K>, V> extends BinarySearchTree<K, V> {
-    // TODO: Implement me!
+    private static final int maxHeightDiff = 1;
+
+    private class AVLNode extends BSTNode {
+        private int height;
+        public AVLNode(K key, V value) {
+            super(key, value);
+            root = null;
+            this.height = 0;
+        }
+    }
+
+    @Override
+    public V insert(K key, V value) {
+        if (key == null || value == null) {
+            throw new IllegalArgumentException();
+        }
+        V result = find(key);
+
+        this.root = insert(key, value, castBSTtoAVL(this.root));
+        castBSTtoAVL(this.root).height = getHeight(castBSTtoAVL(this.root));
+        if (result == null) {
+            this.root = balance(castBSTtoAVL(this.root), key);
+        }
+
+        return result;
+    }
+
+    // Private pair for insert method
+    private AVLNode insert(K key, V newValue, AVLNode currNode) {
+        // Base case for new insertion
+        if(currNode == null) {
+            currNode = new AVLNode(key, newValue);
+            size++;
+            return currNode;
+        }
+
+        int result = key.compareTo(currNode.key);
+        if (result == 0) {
+            // Base case for replacement
+            currNode.value = newValue;
+            return currNode;
+        } else if (result < 0) {
+            // Trace left side
+            currNode.children[0] = insert(key, newValue, castBSTtoAVL(currNode.children[0]));
+        } else if (result > 0) {
+            // Trace right side
+            currNode.children[1] = insert(key, newValue, castBSTtoAVL(currNode.children[1]));
+        }
+        return currNode;
+    }
+
+    // Check the balance of the AVL Tree and rotate if needed
+    private AVLNode balance(AVLNode currNode, K key) {
+        if (currNode.height > 1) {
+            int result = key.compareTo(currNode.key);
+            // Check along the path of insertion from bottom to top
+            if (result < 0 && currNode.children[0] != null) {
+                currNode.children[0] = balance(castBSTtoAVL(currNode.children[0]), key);
+            } else if (result > 0 && currNode.children[1] != null) {
+                currNode.children[1] = balance(castBSTtoAVL(currNode.children[1]), key);
+            }
+            if (getHeight(castBSTtoAVL(currNode.children[1])) - getHeight(castBSTtoAVL(currNode.children[0])) > maxHeightDiff) {
+                if (key.compareTo(currNode.children[1].key) > 0) {
+                    // RR case, rotate left once
+                    currNode = rotateLeft(currNode);
+                } else {
+                    // RL case, rotate right first then rotate left
+                    currNode.children[1] = rotateRight(castBSTtoAVL(currNode.children[1]));
+                    currNode = rotateLeft(currNode);
+                }
+            } else if (getHeight(castBSTtoAVL(currNode.children[0])) - getHeight(castBSTtoAVL(currNode.children[1])) > maxHeightDiff) {
+                if (key.compareTo(currNode.children[0].key) < 0) {
+                    // LL case, rotate right once
+                    currNode = rotateRight(currNode);
+                } else {
+                    // LR case, rotate left first then rotate right
+                    currNode.children[0] = rotateLeft(castBSTtoAVL(currNode.children[0]));
+                    currNode = rotateRight(currNode);
+                }
+            }
+        }
+        return currNode;
+    }
+
+    private AVLNode rotateLeft(AVLNode currNode) {
+        AVLNode oldRoot = currNode;
+        AVLNode temp = null;
+        if (currNode.children[1].children[0] != null) {
+            temp = castBSTtoAVL(currNode.children[1].children[0]);
+        }
+
+        currNode = castBSTtoAVL(currNode.children[1]);
+        oldRoot.children[1] = null;
+        currNode.children[0] = oldRoot;
+        currNode.children[0].children[1] = temp;
+        getHeight(currNode);
+
+        return currNode;
+    }
+
+    private AVLNode rotateRight(AVLNode currNode) {
+        AVLNode oldRoot = currNode;
+        AVLNode temp = null;
+        if (currNode.children[0].children[1] != null) {
+            temp = castBSTtoAVL(currNode.children[0].children[1]);
+        }
+
+        currNode = castBSTtoAVL(currNode.children[0]);
+        oldRoot.children[0] = null;
+        currNode.children[1] = oldRoot;
+        currNode.children[1].children[0] = temp;
+        getHeight(currNode);
+
+        return currNode;
+    }
+
+    // Update and return the height of current node and all nodes below it
+    public int getHeight(AVLNode currNode) {
+        if (currNode == null) {
+            // Base Case
+            return -1;
+        }
+        currNode.height =  Math.max(getHeight(castBSTtoAVL(currNode.children[0])), getHeight(castBSTtoAVL(currNode.children[1]))) + 1;
+        return currNode.height;
+    }
+
+    private AVLNode castBSTtoAVL(BSTNode currNode) {
+        return (AVLNode) currNode;
+    }
 }
+
--- src/main/java/datastructures/worklists/CircularArrayFIFOQueue.java
+++ src/main/java/datastructures/worklists/CircularArrayFIFOQueue.java
@@ -92,25 +92,30 @@ public class CircularArrayFIFOQueue<E extends Comparable<E>> extends FixedSizeFI
 
     @Override
     public int compareTo(FixedSizeFIFOWorkList<E> other) {
-        // You will implement this method in project 2. Leave this method unchanged for project 1.
-        throw new NotYetImplementedException();
+        for (int i = 0; i < Math.min(this.size(), other.size()); i++){
+            if (!this.peek(i).equals(other.peek(i))) {
+                return this.peek(i).compareTo(other.peek(i));
+            }
+        }
+        return this.size() - other.size();
+
+
     }
 
     @Override
     @SuppressWarnings("unchecked")
     public boolean equals(Object obj) {
-        // You will finish implementing this method in project 2. Leave this method unchanged for project 1.
         if (this == obj) {
             return true;
         } else if (!(obj instanceof FixedSizeFIFOWorkList<?>)) {
             return false;
         } else {
-            // Uncomment the line below for p2 when you implement equals
-            // FixedSizeFIFOWorkList<E> other = (FixedSizeFIFOWorkList<E>) obj;
-
-            // Your code goes here
-
-            throw new NotYetImplementedException();
+            FixedSizeFIFOWorkList<E> other = (FixedSizeFIFOWorkList<E>) obj;
+            if (this.compareTo(other) == 0) {
+                return true;
+            } else {
+                return false;
+            }
         }
     }
 
